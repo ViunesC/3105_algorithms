024d9653f94cb71752a476c38164e1fd5767ccec
ViunesC <mrfusking233@gmail.com>
2024-01-24 21:29:08 -0500

    completed Weighted undirected graph ADT

--- WeightedGraph.cpp
+++ WeightedGraph.cpp
@@ -3,3 +3,157 @@
 //
 
 #include "WeightedGraph.h"
+
+namespace WeightedGraph {
+
+    int Edge::getVertexConnected() {
+        return toVertex;
+    }
+
+    int Edge::getWeight() {
+        return weight;
+    }
+
+    Edge::Edge(int v, int weight) {
+        this->toVertex = v;
+        this->weight = weight;
+    }
+
+    int Vertex::getValue() {
+        return value;
+    }
+
+    std::vector<Edge> *Vertex::getEdges() {
+        return &this->edgeList;
+    }
+
+    Vertex::Vertex(int value) {
+        this->value = value;
+    }
+
+    size_t Vertex::getNumConnected() {
+        return edgeList.size();
+    }
+
+    bool Vertex::isConnected(int v) {
+        for (Edge e : edgeList)
+            if (e.getVertexConnected() == v)
+                return true;
+        return false;
+    }
+
+    void Vertex::addEdge(int v, int weight) {
+        Edge e(v, weight);
+        edgeList.push_back(e);
+    }
+
+    void Vertex::removeEdge(int v) {
+        for (auto it=edgeList.begin();it != edgeList.end();++it) {
+            if (it->getVertexConnected() == v) {
+                edgeList.erase(it);
+                return;
+            }
+        }
+        std::cout << "Cannot remove the edge" << std::endl;
+    }
+
+    WeightedGraph::WeightedGraph() = default;
+
+    void WeightedGraph::addVertex(int value) {
+        if (isExist(value)) {
+            std::cout << "Vertex already existed" << std::endl;
+            return;
+        }
+
+        Vertex v(value);
+        adjacencyList.push_back(v);
+    }
+
+    void WeightedGraph::removeVertex(int value) {
+        if (getN() == 0 || !isExist(value))
+            return;
+        for (Vertex v : adjacencyList) {
+            if (v.isConnected(value))
+                removeEdge(value, v.getValue());
+        }
+
+        for (auto it=adjacencyList.begin();it != adjacencyList.end();++it) {
+            if (it->getValue() == value) {
+                adjacencyList.erase(it);
+                break;
+            }
+        }
+    }
+
+    void WeightedGraph::addEdge(int u, int v, int weight) {
+        if (u == v)
+            return;
+
+        int u_pos = find(u);
+        int v_pos = find(v);
+        // std::cout << v_pos << std::endl;
+        if (u_pos == -1 || v_pos == -1) {
+            std::cout << "Vertex " << u << " or " << v << " does not exist" << std::endl;
+            return;
+        }
+        if (adjacencyList[u_pos].isConnected(v)) {
+            std::cout << "Edge already existed" << std::endl;
+            return;
+        }
+        adjacencyList[u_pos].addEdge(v, weight);
+        adjacencyList[v_pos].addEdge(u, weight);
+    }
+
+    void WeightedGraph::removeEdge(int u, int v) {
+        int u_pos = find(u);
+        int v_pos = find(v);
+        if (u_pos == -1 || v_pos == -1) {
+            std::cout << "Vertex " << u << " or " << v << " does not exist" << std::endl;
+            return;
+        }
+        if (!adjacencyList[u_pos].isConnected(v)) {
+            std::cout << "Two vertices are not connected" << std::endl;
+            return;
+        }
+        adjacencyList[u_pos].removeEdge(v);
+        adjacencyList[v_pos].removeEdge(u);
+    }
+
+    size_t WeightedGraph::getN() {
+        return adjacencyList.size();
+    }
+
+    void WeightedGraph::shortest_path(int u) {
+        //TODO: Complete shortest-path-algorithm
+    }
+
+    void WeightedGraph::shortest_path(int u, int v) {
+        //TODO: Complete shortest-path-algorithm
+    }
+
+    bool WeightedGraph::isExist(int value) {
+        for (Vertex v : adjacencyList) {
+            if (v.getValue() == value)
+                return true;
+        }
+        return false;
+    }
+
+    int WeightedGraph::find(int value) {
+        for (int i=0;i<getN();++i) {
+            if (adjacencyList[i].getValue() == value)
+                return i;
+        }
+        std::cout << "Cannot find vertex " << value << std::endl;
+        return -1;
+    }
+
+    void WeightedGraph::printAll() {
+        for (Vertex v : adjacencyList) {
+            std::cout << v.getValue() << ": ";
+            for (Edge e : *v.getEdges())
+                std::cout << e.getVertexConnected() << "[" << e.getWeight() << "] ";
+            std::cout << std::endl;
+        }
+    }
+} // WeightedGraph
--- WeightedGraph.h
+++ WeightedGraph.h
@@ -5,10 +5,54 @@
 #ifndef INC_3105_ALGORITHMS_WEIGHTEDGRAPH_H
 #define INC_3105_ALGORITHMS_WEIGHTEDGRAPH_H
 
+#include <iostream>
+#include <set>
+#include <vector>
 
-class WeightedGraph {
+namespace WeightedGraph {
+    struct Edge {
+    private:
+        int weight;
+        int toVertex;
+    public:
+        Edge(int v, int weight);
+        int getWeight();
+        int getVertexConnected();
+    };
 
-};
+    struct Vertex {
+    private:
+        int value;
+        std::vector<Edge> edgeList;
+    public:
+        Vertex(int value);
+        int getValue();
+        size_t getNumConnected();
+        std::vector<Edge>* getEdges();
+        bool isConnected(int v);
+        void addEdge(int v, int weight);
+        void removeEdge(int v);
+    };
+
+
+    class WeightedGraph {
+    private:
+        std::vector<Vertex> adjacencyList;
+        bool isExist(int value);
+        int find(int value);
+    public:
+        WeightedGraph();
+        void addVertex(int value);
+        void removeVertex(int value);
+        void addEdge(int u, int v, int weight);
+        void removeEdge(int u, int v);
+        size_t getN();
+        void shortest_path(int u);
+        void shortest_path(int u, int v);
+        void printAll();
+    };
+} // WeightedGraph
+
 
 
 #endif //INC_3105_ALGORITHMS_WEIGHTEDGRAPH_H
